8dd34d3dffccd4ef9f738d870be8d903560db92e
Maximillian Kirchoff <max@lamasix.com>
2020-11-10 06:25:19 -0600

    updated my site

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-printf '\n\n Syncing to S3 from local "public" folder.\n Excluding any dot files or folders.\n\n'
-aws s3 sync app/ s3://www.maxelby.com --acl public-read --exclude=".*" --delete
